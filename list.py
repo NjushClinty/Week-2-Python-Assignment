414027c2e9eb0b7c14564af0c572a7533211556e
NjushClinty <clintkash100@students.uonbi.ac.ke>
2024-08-31 22:36:34 +0300

    Solution to Week 2 Python Assignment

---
+++ list.py
@@ -0,0 +1,28 @@
+my_list = []
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+print(my_list)
+
+my_list.insert(1,15)
+print(my_list)
+
+#Extending a list
+list2 = [50,60,70]
+print(list2)
+my_list.extend(list2)
+print(my_list)
+
+#Remove from list
+last_element = my_list.pop()
+print(my_list)
+
+#Sorting list
+my_list.sort()
+print(my_list)
+
+#Finding index of element
+index_of_30 = my_list.index(30)
+print(index_of_30)
